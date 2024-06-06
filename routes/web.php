5b9774afd64647a92f3f1869e7b8bcaee334c877
suzt-0 <suztbhttr2@gmail.com>
2024-06-06 12:20:23 +0545

    remove routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function () {
-    return view('test');
-});
-
-Route::get('/task', function () {
-    return view('task');
-});
