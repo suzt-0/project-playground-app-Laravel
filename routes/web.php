78a9b370e49c3c9def10341c29e0325f61323df6
suzt-0 <suztbhttr2@gmail.com>
2024-06-10 20:10:14 +0545

    add route groups and UserController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectMemberController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(ProjectController::class)->group(function () {
+    // add routes related to ProjectController here
+});
+
+Route::controller(ProjectMemberController::class)->group(function () {
+    // add routes related to ProjectMemberController here
+});
+
+Route::controller(TaskController::class)->group(function () {
+    // add routes related to TaskController here
+});
+
+Route::controller(CommentController::class)->group(function () {
+    // add routes related to CommentController here
+});
+
+Route::controller(UserController::class)->group(function () {
+    // add routes related to UserController here
+});
+
+
