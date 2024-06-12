7226a1c45ccf0a90186f007411dbb570a81566fc
suzt-0 <suztbhttr2@gmail.com>
2024-06-12 13:11:44 +0545

    add animation and view files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Project-Playground</title>
     @vite('resources/css/app.css')
+    @vite('resources/css/dashboard.css')
 </head>
 
 <body>
@@ -21,29 +22,38 @@
                 <span class="ml-3 text-xl">Project Management System</span>
             </a>
             <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                {{-- added a underline upon a hover --}}
                 <a href="#" class="mr-5 hover:text-gray-900 hover:underline hover:decoration-gray-900">About</a>
                 <a href="#" class="mr-5 hover:text-gray-900 hover:underline hover:decoration-gray-900">Contacts</a>
                 <a href="#" class="mr-5 hover:text-gray-900 hover:underline hover:decoration-gray-900">User Manual</a>
             </nav>
         </div>
     </header>
+
+
     <section class="text-gray-700 body-font">
         <div class="container mx-auto flex px-5 py-24 flex-col md:flex-row items-center">
-            <div
-                class="flex flex-col mb-16 items-center text-center md:w-1/2 md:pr-16 md:items-start md:text-left md:mb-0 lg:flex-grow">
+
+            <div class="flex flex-col mb-16 items-center text-center md:w-1/2 md:pr-16 md:items-start md:text-left md:mb-0 lg:flex-grow">
+
                 <h1 class="title-font text-3xl mb-4 font-medium text-gray-900 sm:text-4xl">Project
                     <br class="hidden lg:inline-block">Management System
                 </h1>
-                <p class="mb-8 leading-relaxed">Sample text here Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit. Facilis debitis voluptatibus nesciunt dolorem ducimus, a perferendis nemo, dolores
-                    consequuntur sed culpa cumque ad eveniet et esse odit ipsa doloremque neque?</p>
+               {{-- Tried adding a typing animations --}}
+                   <h2><q class="mb-8 leading-relaxed p-0 m-0 typewriter max-w-fit"> Learn to Manage your Projects more efficiently with Project Management System</q></h2>
                 <div class="flex justify-center">
-                    <button
-                        class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 rounded text-lg focus:outline-none hover:bg-indigo-600">Signup</button>
-                    <button
-                        class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 rounded text-lg focus:outline-none hover:bg-gray-300">Login</button>
+
+                    <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 rounded text-lg focus:outline-none hover:bg-indigo-600">
+                        Signup
+                    </button>
+
+                    <button class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 rounded text-lg focus:outline-none hover:bg-gray-300">
+                        Login
+                    </button>
+
                 </div>
             </div>
+
             <div class="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
                 <img class="object-cover object-center rounded" src="https://dummyimage.com/720x600/edf2f7/a5afbd">
             </div>
