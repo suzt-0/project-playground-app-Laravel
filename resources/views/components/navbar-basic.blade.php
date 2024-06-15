c9d0772232abf51e927440988b3a23d34c392390
suzt-0 <suztbhttr2@gmail.com>
2024-06-15 15:48:34 +0545

    add a blade component

---
+++ resources/views/components/navbar-basic.blade.php
@@ -0,0 +1,19 @@
+<header class="text-gray-700 body-font border-b border-gray-200">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="{{route('home')}}"">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                viewBox="0 0 24 24">
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-xl">Project Management System</span>
+        </a>
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center p-1">
+            <a href="{{route('home')}}" class="mr-5 p-1 hover:text-gray-900 hover:underline hover:decoration-gray-900">Home</a>
+            <a href="#" class="mr-5 p-1 hover:text-gray-900 hover:underline hover:decoration-gray-900">About</a>
+            <a href="#" class="mr-5 p-1 hover:text-gray-900 hover:underline hover:decoration-gray-900">Contacts</a>
+            <a href="#" class="mr-5 p-1 hover:text-gray-900 hover:underline hover:decoration-gray-900">User
+                Manual</a>
+        </nav>
+    </div>
+</header>
