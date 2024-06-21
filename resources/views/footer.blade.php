5bb0e2746a9eaf28b71f8cb3ae97693910815856
suzt-0 <suztbhttr2@gmail.com>
2024-06-21 23:21:23 +0545

    create a footer template

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+
+<section class="text-right font-semibold text-gray-500">
+    <a href="#" class="text-purple-600 hover:underline">Recreated by Mozia Precious Chibundu &#10083;</a> with <a
+      href="https://tailwindcss.com/" class="text-teal-400 hover:underline">Tailwind CSS</a>
+
+  </section> --}}
