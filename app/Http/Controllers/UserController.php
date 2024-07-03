7ecf185644335ec128e07669d8135dc462383b23
suzt-0 <suztbhttr2@gmail.com>
2024-07-03 22:58:49 +0545

    simplifying user controller and adding user authenticated access

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
@@ -19,6 +20,7 @@ class UserController extends Controller
         // return view('');
     }
 
+
     /**
      * Show the form for creating a new resource.
      */
@@ -32,21 +34,70 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-       
-        $request->validate([
+        $data =$request->validate([
             'name' => 'required|string',
             'email' => 'required|string',
-            'password' => 'required|string',
+            'password' => 'required|string|confirmed',
             
         ]);
+            $user = User::create($data);
 
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
-        return redirect()->route('login')->with('success', 'User data added successfully!');
+            if($user){
+                return redirect()->route('login')->with('success', 'User data added successfully!');
+            }
+        //$request->validate([
+        //     'name' => 'required|string',
+        //     'email' => 'required|string',
+        //     'password' => 'required|string|confirmed',  
+        // ]);
+        // $user = new User();
+        // $user->name = $request->name;
+        // $user->email = $request->email;
+        // $user->password = $request->password;
+        // $user->save();
+        // return redirect()->route('login')->with('success', 'User data added successfully!');
     }
+
+
+    // Defining logic for Login Function
+
+    // Show login form
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    //Authentication of login info
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // $request->session()->regenerate();
+
+            return redirect()->route('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function dashboardPage(){
+        if(Auth::check()){
+            return view('dashboard');
+        }
+        else{
+            return redirect()->route('login');
+        }
+    }
+
 
     /**
      * Display the specified resource.
