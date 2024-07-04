594c7072705e1b5a113b869aad7c69b2e177c73f
suzt-0 <suztbhttr2@gmail.com>
2024-07-04 22:10:32 +0545

    add dashboard controller to handle dashboard logic

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        $myProject = $user->project()->count();
+        $completedProject = $user->project()->whereStatus('Completed')->count();
+        $failedProject = $user->project()->whereStatus('Dropped')->count();
+        // need modification
+        $joinedProject = $user->project()->count();
+        return view('dashboard', compact('user', 'myProject','joinedProject','completedProject','failedProject'));
+
+        // $project = $user->projects()->get();
+        // return view('dashboard', compact('user', 'projectCount','project'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
