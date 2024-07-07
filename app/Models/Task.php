de8ac8137173f66952e198f39a2afcc3fc93c0b2
suzt-0 <suztbhttr2@gmail.com>
2024-07-07 08:26:55 +0545

    add guarded for non-changeable fields of data

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,6 +10,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $guarded=[
+        'id',
+        'created_at',
+        'updated_at',
+    ];
     public function project() :BelongsTo{
         return $this->belongsTo(Project::class, 'project_id','id');
     }
