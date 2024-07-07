5ce9d4c4453ba86a91f463e0037434573db36c35
suzt-0 <suztbhttr2@gmail.com>
2024-07-07 08:28:01 +0545

    define relationship with project and project member to realize myProjects and joinProjects features

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -45,9 +46,21 @@ class User extends Authenticatable
         ];
     }
 
-    public function project()
+    public function myProject(): HasMany
     {
-        return $this->hasMany(Project::class);
-        // return $this->belongsToMany(Project::class, 'project_members', 'user_id', 'project_id');
+        // return $this->hasMany(Project::class);
+        // above code searches for User_is 
+        // ie modelname_id and as we changed user_id to admin_id in migration the controller threw error 
+        return $this->hasMany(Project::class, 'admin_id', 'id');
+    }
+
+    public function joinedProject()
+    {
+        return $this->belongsToMany(Project::class, 'project_members', 'user_id', 'project_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'user_id');
     }
 }
