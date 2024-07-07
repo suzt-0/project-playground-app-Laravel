bcf0e947c3cbcd80de083a71f7dc3888c9f3f78f
suzt-0 <suztbhttr2@gmail.com>
2024-07-07 08:25:30 +0545

    define relationship between project & user and project & project_members

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,22 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
     use HasFactory;
-    public function task() :HasMany{
+
+    protected $guarded=[
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function tasks() :HasMany{
         return $this->hasMany(Task::class, 'project_id','id');
     }
 
-    public function projectmember() :HasMany{
-        return $this->hasMany(ProjectMember::class, 'project_id','id');
-    }
+    // public function projectmember() :HasMany{
+    //     return $this->hasMany(ProjectMember::class, 'project_id','id');
+    // }
 
-    public function user() :BelongsTo
+    public function admin() :BelongsTo
     {
-    return $this->belongsTo(User::class);
-
+        return $this->belongsTo(User::class);//admin
+    }
+    public function users() : BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'project_members', 'project_id', 'user_id');
     }
 }
