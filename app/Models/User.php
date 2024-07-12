0ad47a90e187478d4c6d249ba8fe7b2213c4f9b8
suzt-0 <suztbhttr2@gmail.com>
2024-07-12 23:48:59 +0545

    add relatonship function

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class, 'user_id');
     }
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class, 'project_members', 'user_id', 'project_id');
+    }
 }
