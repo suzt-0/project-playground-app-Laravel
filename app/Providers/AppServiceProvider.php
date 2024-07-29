9ea35e748391c7a788d007edd5700624e7b5ac93
suzt-0 <suztbhttr2@gmail.com>
2024-07-29 22:27:49 +0545

    add policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,24 @@
 
 namespace App\Providers;
 
+use App\Models\Project;
+use App\Models\Task;
+use App\Policies\ProjectPolicy;
+use App\Policies\TaskPolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    // /**
+    //  * The policy mappings for the application.
+    //  *
+    //  * @var array
+    //  */
+    // protected $policies = [
+    //     Task::class => TaskPolicy::class,
+    //     Project::class => ProjectPolicy::class,
+    // ];
+
     /**
      * Register any application services.
      */
