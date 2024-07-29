37c8501af5d170c18ed0e263d51c8de72001556e
suzt-0 <suztbhttr2@gmail.com>
2024-07-29 23:03:24 +0545

    add emailcontroller (forgot to commit initally)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\EmailController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ProjectMemberController;
 use App\Http\Controllers\DashboardController;
