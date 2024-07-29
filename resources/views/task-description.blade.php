ef9e6a381d8f2cd53a8c4af0a8a0be7f61ff2377
suzt-0 <suztbhttr2@gmail.com>
2024-07-29 23:27:49 +0545

    add notify and css changes

--- resources/views/task-description.blade.php
+++ resources/views/task-description.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>TEST-Project Description</title>
+  @notifyCss
   @vite('resources/css/app.css')
 </head>
 
@@ -13,6 +14,7 @@
   {{-- navbar starts --}}
   @include('components.navbar')
   {{-- navbar ends --}}
+
   @if ($errors->any())
   <div>
     <ul>
@@ -33,17 +35,18 @@
     {{ session('success') }}
   </div>
   @endif
+
   <main class="flex-1 flex flex-col text-slate-700 gap-4 p-4 md:gap-8 md:p-10">
     {{-- back links --}}
-    <div>
+    {{-- <div>
       dashboard/
       {{$task->project->name}}/
       {{$task->name}}/..
-    </div>
+    </div> --}}
 
-    <div class="w-full grid md:grid-cols-3 gap-8">
+    <div class="w-full grid grid-cols-4 xl:grid-cols-5 gap-8">
       {{-- Task Card Starts --}}
-      <div class="rounded-lg border md:col-span-2 bg-slate-50 shadow-sm">
+      <div class="rounded-lg border col-span-4 xl:col-span-3 bg-slate-50 shadow-sm">
         <div class="flex flex-col space-y-1.5 p-5">
           <h3 class="whitespace-nowrap text-3xl font-semibold leading-none tracking-tight">{{$task->name}}
         </div>
@@ -51,7 +54,7 @@
           <div class="grid md:grid-cols-1 gap-4">
             <div class="grid-cols-1">
               <div class="text-xl font-medium">Description</div>
-              <div class="text-base rounded p-2 border h-56 overflow-y-auto">{{$task->description}}</div>
+              <div class="text-base rounded p-2 border h-40 overflow-y-auto">{{$task->description}}</div>
             </div>
           </div>
           <div class="grid grid-cols-2 gap-4">
@@ -85,7 +88,7 @@
               </div>
             </div>
           </div>
-          <div class="grid grid-cols-2 gap-4">
+          <div class="grid grid-cols-1 gap-4">
             <div>
               <div class="text-sm font-medium">Assigned to</div>
               <div class="text-base">
@@ -100,6 +103,7 @@
                 @endif
               </div>
             </div>
+            @if (Auth::user()->id == $task->project->admin_id)
             <div>
               <div class="items-center p-3 flex justify-end gap-2">
                 {{-- Delete button --}}
@@ -146,6 +150,7 @@
                 </a>
               </div>
             </div>
+            @endif
           </div>
         </div>
       </div>
@@ -274,9 +279,10 @@
   </main>
 
   {{-- footer starts --}}
-  @include('components.footer')
+  {{-- @include('components.footer') --}}
   {{-- footer ends --}}
-
+  <x-notify::notify />
+  @notifyJs
 </body>
 
 </html>
