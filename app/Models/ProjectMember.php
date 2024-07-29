7db85bcf045e164ffc22dc1cf900abef22f206be
suzt-0 <suztbhttr2@gmail.com>
2024-07-29 23:01:23 +0545

    add member to task relationship(hasMany)

--- app/Models/ProjectMember.php
+++ app/Models/ProjectMember.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProjectMember extends Model
 {
@@ -30,6 +31,10 @@ class ProjectMember extends Model
     
     public function projects() : BelongsToMany {
         return $this->belongsToMany(Project::class, 'project_members', 'user_id', 'project_id');
+    }
+
+    public function tasks() :HasMany{
+        return $this->hasMany(Task::class, 'user_id','id');
     }
     
 }
